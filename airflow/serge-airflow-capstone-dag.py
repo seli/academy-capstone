675caec26d697e363c649bbd99719a0d8b76f54e
Serge Liberloo <serge.liberloo@aselia.be>
2023-03-27 09:29:41 +0000

    fix cast datatime

--- airflow/serge-airflow-capstone-dag.py
+++ airflow/serge-airflow-capstone-dag.py
@@ -4,7 +4,7 @@ from airflow.providers.amazon.aws.operators.batch import BatchOperator
 
 serge_capstone_batch_job = BatchOperator(
     task_id = 'serge_pxl_batch_job',
-    job_name = 'serge_pxl_batch_job-' + pendulum.now(),
+    job_name = 'serge_pxl_batch_job-' + str(pendulum.now()),
     job_queue = 'academy-capstone-pxl-2023-job-queue',
     job_definition = 'serge-pxl-capstone',
     overrides = {}
