d6be2f3105b5e3fee2644479401034b88dc93256
Serge Liberloo <serge.liberloo@aselia.be>
2023-03-27 09:35:53 +0000

    define the with DAG task

--- airflow/serge-airflow-capstone-dag.py
+++ airflow/serge-airflow-capstone-dag.py
@@ -2,10 +2,16 @@ import pendulum
 from airflow import DAG
 from airflow.providers.amazon.aws.operators.batch import BatchOperator
 
-serge_capstone_batch_job = BatchOperator(
-    task_id = 'serge_pxl_batch_job',
-    job_name = 'serge_pxl_batch_job-' + str(pendulum.now()),
-    job_queue = 'academy-capstone-pxl-2023-job-queue',
-    job_definition = 'serge-pxl-capstone',
-    overrides = {}
-)
+with DAG(
+    dag_id='serge_pxl_batch_job',
+    schedule_interval='@once',
+    start_date=datetime(2023, 3, 27),
+    catchup=False,
+) as serge_pxl_batch_job:
+    serge_capstone_batch_job = BatchOperator(
+        task_id = 'serge_pxl_batch_job',
+        job_name = 'serge_pxl_batch_job-' + str(pendulum.now()),
+        job_queue = 'academy-capstone-pxl-2023-job-queue',
+        job_definition = 'serge-pxl-capstone',
+        overrides = {}
+    )
