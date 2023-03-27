27dbd414809f752611a77e503cc5cca08251097e
Serge Liberloo <serge.liberloo@aselia.be>
2023-03-27 10:02:44 +0000

    add documentation

--- airflow/serge-airflow-capstone-dag.py
+++ airflow/serge-airflow-capstone-dag.py
@@ -15,8 +15,8 @@ with DAG(
 ) as serge_pxl_batch_job:
     serge_capstone_batch_job = BatchOperator(
         task_id = 'serge_pxl_batch_job',
-        job_name = 'serge_pxl_batch_job-' + str(current_milli_time()),
-        job_queue = 'academy-capstone-pxl-2023-job-queue',
-        job_definition = 'serge-pxl-capstone',
+        job_name = 'serge_pxl_batch_job-' + str(current_milli_time()), # unique name per running batch -> can be found in AWS Batch - Job
+        job_queue = 'academy-capstone-pxl-2023-job-queue', # name used in AWS Batch - Job Queue 
+        job_definition = 'serge-pxl-capstone', # name used in AWS Batch -  Job Definition
         overrides = {}
     )
